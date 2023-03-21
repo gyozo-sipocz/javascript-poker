8011ab69218bff447448b9d6dd038c2a6b1844bd
gyozo-sipocz <sipoczgy@yahoo.com>
2023-03-21 20:43:51 +0100

    slider component

--- poker.js
+++ poker.js
@@ -2,13 +2,46 @@ const newGameButton = document.querySelector('.js-new-game-button');
 const playerCardsContainer = document.querySelector('.js-player-cards-container');
 const chipCountContainer = document.querySelector('.js-chip-count-container');
 const potContainer = document.querySelector('.js-pot-container');
+const betArea = document.querySelector('.js-bet-area');
+const betSlider = document.querySelector('#bet-amount');
+const betSliderValue = document.querySelector('.js-slider-value');
 
 // Program State
-let deckID = null;
-let playerCards = [];
-let playerChips = 100;
-let computerChips = 100;
-let pot = 0;
+let {
+  deckID,
+  playerCards,
+  playerChips,
+  computerChips,
+  pot
+} = getInitialState();
+
+function getInitialState() {
+  return {
+    deckID: null,
+    playerCards: [],
+    playerChips: 100,
+    computerChips: 100,
+    pot: 0
+  };
+}
+
+function initialize() {
+  ({deckID, playerCards, playerChips, computerChips, pot} = getInitialState());
+}
+
+function canBet() {
+  return playerCards.length === 2 && playerChips > 0 && pot === 0;
+}
+
+function renderSlider() {
+  if (canBet()) {
+    betArea.classList.remove('invisible');
+    betSlider.setAttribute('max', playerChips);
+    betSliderValue.innerText =  betSlider.value;
+  } else {
+    betArea.classList.add('invisible');
+  }
+}
 
 function renderPlayerCards() {
   let html = '';
@@ -36,6 +69,7 @@ function render() {
   renderPlayerCards();
   renderChips();
   renderPot();
+  renderSlider();
 }
 
 function drawAndRenderPlayerCards() {
@@ -58,3 +92,6 @@ function startGame() {
 }
 
 newGameButton.addEventListener('click', startGame);
+betSlider.addEventListener('change', render);
+initialize();
+render();
